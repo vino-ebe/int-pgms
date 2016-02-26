d9c2749c64d9f6ca31f2039f09ed47b0dabe9d2d
Vinoj Stanley <vino.ebe@outlook.com>
2016-02-26 00:33:01 -0800

    BFS and DFS Implementation

--- skiena/bfs.cc
+++ skiena/bfs.cc
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<queue>
+
 using namespace std;
 
 typedef struct edgeNode {
@@ -19,12 +21,23 @@ typedef struct graphs {
 class graph {
     private:
         graph_t G;
+        int state[4];
+        int parent[4];
+
+        enum stateVar {
+            kUnDiscovered,
+            kDiscovered,
+            kProcessed
+        }stateEnum;
 
     public:
         graph() {
             for (int i = 0; i < MAXV; i++) {
                 G.node[i] = NULL;
                 G.degree[i] = 0;
+            }
+            for (int i = 0; i < 4; i++) {
+                state[i] = kUnDiscovered;
             }
             G.directed = true;
             G.nVertex = 0;
@@ -55,17 +68,70 @@ class graph {
                 }
             }
         }
+
+        void bfs(int startVertex) {
+            queue<int> edgeQueue;
+
+
+            for (int i = 0; i < 4; i++) {
+                state[i] = kUnDiscovered;
+                parent[i] = -1;
+            }
+
+            state[startVertex] = kDiscovered;
+            parent[startVertex] = -1;
+
+            edgeQueue.push(startVertex);
+
+            while (!edgeQueue.empty()) {
+                int v = edgeQueue.front();
+                edgeQueue.pop();
+                state[v] = kProcessed;
+                cout<<v<<endl;
+                edgeNode_t *curEdge = G.node[v];
+                // Add all its neighbours to the queue
+                while (curEdge != NULL) {
+                    if (state[curEdge->v] != kDiscovered && state[curEdge->v] != kProcessed) { 
+                        state[curEdge->v] = kDiscovered;
+                        edgeQueue.push(curEdge->v);
+                    }
+                    curEdge = curEdge->next;
+                }
+            }
+        }
+        
+        void dfs(int startVertex) {
+
+            state[startVertex] = kDiscovered;
+            cout<<startVertex<<endl;
+            edgeNode_t *curEdge = G.node[startVertex];
+
+            while (curEdge != NULL) {
+                if (state[curEdge->v] != kDiscovered && state[curEdge->v] != kProcessed) {
+                    state[curEdge->v] = kDiscovered;
+                    parent[curEdge->v] = startVertex;
+                    dfs(curEdge->v);
+                }
+                curEdge = curEdge->next;
+            }
+            cout<<"Setting vertex "<<startVertex<<"Processed"<<endl;
+            state[startVertex] = kProcessed;
+        }
 };
 
 int main()
 {
-    graph gr;
-    gr.addEdge(0,1);
-    gr.addEdge(0,3);
-    gr.addEdge(1,3);
-    gr.addEdge(2,3);
-    gr.addEdge(2,1);
+    graph *gr = new graph();
+    gr->addEdge(0,3);
+    gr->addEdge(1,0);
+    gr->addEdge(1,2);
+    gr->addEdge(2,3);
+    gr->addEdge(3,0);
+    gr->addEdge(3,1);
 
-    gr.printGraph();
+    //gr->bfs(0);
+    cout<<"DFS"<<endl;
+    gr->dfs(0);
+    gr->printGraph();
     return 0;
 }
